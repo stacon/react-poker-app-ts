bd219b1287fa0cd11b6f6040b8d1d3d73b191159
BreJohn <i.bregiannis@gmail.com>
2019-01-09 13:42:19 +0200

    Css stuff fix
    - 4 player game

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -18,28 +18,37 @@ const mapStateToProps = (state: IState): { [key: string]: IPlayer[] } => {
 
 const Board = (props: any): JSX.Element => {
 
-  const { players } = props;
-  let playersOrdering = "inner-wrapper";
-
-  if (players.length % 2 !== 0) {
-    playersOrdering += " players-ordering-3"
-  }
+  let { players } = props;
+  // let playersOrdering = "inner-wrapper";
+  // if (players.length % 2 !== 0) {
+  //   playersOrdering += " grid"
+  //   // players = players.slice(0).reverse();
+  // }
   
+
   return (
     <section className="board_wrapper">
       <div className="top_buttons_wrapper">
         <Button
-          btnClasses={ 'btn btn-warning' }
-          btnText={ 'Leave Table' }
-          btnHandler={ null }
+          btnClasses={'btn btn-warning'}
+          btnText={'Leave Table'}
+          btnHandler={null}
         />
       </div>
-      
-      <div className={playersOrdering}>
-     
-        {
-          players.map( (p: IPlayer, i: number) => <Player key={i} playerObj={p}/> )
-        }
+
+      <div className="inner-wrapper grid">
+        <div className="grid-item"></div>
+        <div className="grid-item"><Player key={players[3].index} playerObj={players[3]} /></div>
+        <div className="grid-item"></div>
+        <div className="grid-item"><Player key={players[1].index} playerObj={players[1]} /></div>
+        <div className="grid-item"></div>
+        <div className="grid-item"><Player key={players[2].index} playerObj={players[2]} /></div>
+        <div className="grid-item"></div>
+        <div className="grid-item"><Player key={players[0].index} playerObj={players[0]} /></div>
+        <div className="grid-item"></div>
+          {/* {
+            players.map((p: IPlayer, i: number) => <Player key={i} playerObj={p} />)
+          } */}
       </div>
       <div className="status-wrapper">
         <div className="inner-wrapper">
