8ae1c1c4c645cba5563b0d83e283d426eb5a5d29
BreJohn <i.bregiannis@gmail.com>
2019-01-15 11:14:02 +0200

    Merge

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -25,54 +25,10 @@ const Board = (props: any): JSX.Element => {
   //   // players = players.slice(0).reverse();
   // }
 
-  const gridItems: JSX.Element[] = Array(9).fill(undefined);
+  if (players.length % 2 !== 0) {
+    playersOrdering += " players-ordering-3"
+  }
   
-  const playersGrid: JSX.Element[] = gridItems.map((gridItem, index) => {
-
-    if(index === 1 && players.length >= 2 ) {
-      return (
-        <div className={"grid-item"}>
-          <Player key={players[1].index} playerObj={players[1]} />
-        </div>
-      )
-    }
-
-    else if(index === 3 && players.length >= 3 ) {
-      return (
-        <div className={"grid-item"}>
-          <Player key={players[2].index} playerObj={players[2]} />
-        </div>
-      )
-    }
-    
-    else if(index === 5 && players.length === 4) {    
-    return (
-      <div className={"grid-item"}>
-        <Player key={players[3].index} playerObj={players[3]} />
-      </div>
-    )
-    }
-
-
-    else if(index === 7 && players.length >= 1 ) {
-      return (
-        <div className={"grid-item"}>
-          <Player key={players[0].index} playerObj={players[0]} />
-        </div>
-      )
-    }
-    else {
-      return (
-        <div className={"grid-item"}></div>
-      )
-    }
-  })
-
-
-  let playersLength = players.length-1;
-  console.log(playersLength);
-
-
   return (
     <section className="board_wrapper">
       <div className="top_buttons_wrapper">
