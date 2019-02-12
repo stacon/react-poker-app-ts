872b79db017665f7b10f2a0b5e9da7e2d6208a4d
Ioannis Bregiannis <i.bregiannis@gmail.com>
2019-02-12 13:47:19 +0200

    Fix for redirection to home

--- src/components/Views/Game/Game.tsx
+++ src/components/Views/Game/Game.tsx
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Board from './Board/Board';
+import { connect } from 'react-redux';
+import { AppState } from 'src/models/App/app.store';
+import { GameState } from 'src/models/Game/game.reducer';
+import { history } from "../../Routes";
 
-class Game extends Component {
-  render() {
-    return <Board/>
-  }
+
+interface Props {
+  game: GameState
 }
 
-export default Game
+export const game = ({game} : Props) => {
+  console.log(game);
+  if (Object.keys(game).length === 0 && game.constructor === Object) {
+    history.push('/');
+    return <></>
+  }
+   return <Board/>;
+}
+
+const mapStateToProps = (state: AppState) => {
+  return {
+    game: state.game
+  }
+};
+
+export default connect(mapStateToProps)(game);
