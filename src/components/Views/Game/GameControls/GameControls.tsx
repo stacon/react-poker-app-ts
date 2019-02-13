452aa95417d920e529a8c96055bf7a3ef5e6589d
Ioannis Bregiannis <i.bregiannis@gmail.com>
2019-02-13 11:22:09 +0200

    Edit game controls

--- src/components/Views/Game/GameControls/GameControls.tsx
+++ src/components/Views/Game/GameControls/GameControls.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
 import './GameControls.module.css';
+import { AppState } from 'src/models/App/app.store';
+import { connect } from 'react-redux';
+import { raise, changeRaiseAmount } from 'src/models/Game/game.actions.creator';
 
-// interface Props {
+interface Props {
+  balance: number,
+  onChangeRaiseAmount: Function,
+  amountForRaise: number,
+  onRaise: Function
+}
 
-// }
+export const gameControls = (
+  { balance, onChangeRaiseAmount, amountForRaise, onRaise }: Props
+) => { 
+return (
+    <div className="game-control-container">      
+      <li>Fold</li>
+      <li>Check</li>
+      <li><input
+        className="raise-range"
+        id='raise'
+        type="range"
+        min="1"
+        max={balance}
+        step="1"
+        value={amountForRaise}
+        onChange={(event) => onChangeRaiseAmount(+event.target.value)}
+      >
+      </input>
+      <div>{amountForRaise.toFixed(2)}</div>
+      <div className="raise" onClick = {() => onRaise()} >Raise</div></li>
+      <li>Replace Cards</li>
+    </div>
+  )
+}
+const mapStateToProps = (state: AppState) => {
+  return {
+    balance: (state.game.players) ? state.game.players[0].balance : 0,
+    amountForRaise: state.game.amountForRaise
+  }
+};
 
-export const gameControls = () => (
-  <div className="game-control-container">
-    <li>Bet</li>
-    <li>Call</li>
-    <li>Raise</li>
-    <li>Replace Cards</li>
-    <li>Discard</li>
-  </div>
-)
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    onRaise: () => {
+      dispatch(raise());
+    },
+    onChangeRaiseAmount: (amount: number): void => {
+      dispatch(changeRaiseAmount(amount));
+  },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(gameControls);
