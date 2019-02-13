80bf832d9cfcee50faf91e0c15e23b7b76c40d69
Ioannis Bregiannis <i.bregiannis@gmail.com>
2019-02-13 11:32:15 +0200

    Edit message reducer

--- src/models/Messages/messages.action.creator.ts
+++ src/models/Messages/messages.action.creator.ts
@@ -1,12 +1,20 @@
 // #region Actions
-export const SHOW_HAND_VALUE = 'SHOW_HAND_VALUE';
+export const ADD_MESSAGE = 'ADD_MESSAGE';
 export const INITIALIZE_MESSAGES = 'INITIALIZE_MESSAGES';
+
 // #endregion Actions
 
 // #region Action Creators
 export const initializeMessages = () => {
   return {
     type: INITIALIZE_MESSAGES
+  }
+}
+
+export const addMessage = (message: String) => {
+  return { 
+    type: ADD_MESSAGE,
+    payload: {message}
   }
 }
 // #endregion Action Creators
--- src/models/Messages/messages.reducer.ts
+++ src/models/Messages/messages.reducer.ts
@@ -1,4 +1,5 @@
-import { START_GAME } from '../Game/game.actions.creator';
+import { DEAL_CARDS } from '../Game/game.actions.creator';
+import { ADD_MESSAGE } from './messages.action.creator';
 
 export interface MessagesState {
   list?: string[]
@@ -6,11 +7,18 @@ export interface MessagesState {
 
 export default function (state: MessagesState = {list: []}, action: any) {
   switch (action.type) {
-    case (START_GAME): {
-      console.log('Mpike mesa');
+    case (DEAL_CARDS): {
+      const list=  (state.list) ? [...state.list, 'HANDS DEALT! GOOD LUCK'] : [];      
       return {
         ...state,
-        list: []
+        list
+      }
+    }
+    case (ADD_MESSAGE): {
+      const list=  (state.list) ? [...state.list, action.payload.message] : [];      
+      return {
+        ...state,
+        list
       }
     }
     default: {
