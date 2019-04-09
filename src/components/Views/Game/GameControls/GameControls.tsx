77914709d130c9f9ba0454cea917632ff5e09c8c
Constantinos Stathis <constantinos-@hotmail.com>
2019-04-09 13:19:03 +0300

    Added more game selector and applied to GameControls.tsx

--- src/components/Views/Game/GameControls/GameControls.tsx
+++ src/components/Views/Game/GameControls/GameControls.tsx
@@ -3,8 +3,8 @@ import './GameControls.module.css';
 import { AppState } from 'src/models/App/app.store';
 import { connect } from 'react-redux';
 import { raise, changeRaiseAmount, call, check, replaceCards } from 'src/models/Game/game.actions.creator';
-import { UICard } from 'src/types';
 import { GameStatus } from 'src/enums';
+import { getGameStatus, getMainPlayer, getGameAmountForRaise, getSelectedCardsForReplacementNumber } from 'src/models/Game/game.selectors';
 
 interface Props {
   balance: number,
@@ -47,11 +47,10 @@ export const gameControls = (
 
 const mapStateToProps = (state: AppState) => {
   return {
-    balance: (state.game.players) ? state.game.players[0].balance : 0,
-    amountForRaise: state.game.amountForRaise,
-    status: state.game.status,
-    selectedCardsForReplacement: state.game.players && state.game.players[0].hand ?
-          state.game.players[0].hand.filter((card: UICard) => card.selected).length : null
+    balance: !!getMainPlayer(state).balance ? getMainPlayer(state).balance : 0,
+    amountForRaise: getGameAmountForRaise(state),
+    status: getGameStatus(state),
+    selectedCardsForReplacement: getSelectedCardsForReplacementNumber(state),
   }
 };
 
--- src/models/Game/game.selectors.ts
+++ src/models/Game/game.selectors.ts
@@ -1,5 +1,5 @@
 import { AppState } from "../App/app.store";
-import { GameState, IPlayer } from 'src/types';
+import { GameState, IPlayer, UICard } from 'src/types';
 import { GameStatus } from 'src/enums';
 
 export const getGameState = (state: AppState): GameState => state.game;
@@ -13,7 +13,13 @@ export const gameHasStarted = (state: AppState): boolean => (
     getGameStatus(state) > GameStatus._NewGame
 );
 
+// By ref
 export const getGamePlayers = (state: AppState): IPlayer[] => [...getGameState(state).players];
+export const getMainPlayer = (state: AppState): IPlayer => ({...getGameState(state).players[0]});
+
+// By val
 export const getGameStatus = (state: AppState): GameStatus => getGameState(state).status;
 export const getGamePot = (state: AppState): number => getGameState(state).pot;
 export const getGameDealerIndex = (state: AppState): number => getGameState(state).dealerIndex;
+export const getGameAmountForRaise = (state: AppState): number => getGameState(state).amountForRaise;
+export const getSelectedCardsForReplacementNumber = (state: AppState): number => getMainPlayer(state).hand.filter((card: UICard) => card.selected).length;
